a18b676bdccd0de1c3baa68b4332fac1f34b4ff3
Nicolle <nickkoetzler@email.com>
2022-05-09 16:23:47 -0300

    termina itens dinâmicos

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -3,20 +3,51 @@ import Usuario from "./Usuario";
 
 
 export default function Sidebar(){
+
+
+    const itemsUsuario = [
+      {
+        name: "Catana",
+        boldname: "catanacomics",
+        image: "assets/img/catanacomics.svg",
+      }
+    ]
+
+
+    const itemsSugestao = [
+      {
+        title: "bad.vibes.memes",
+        src: "assets/img/bad.vibes.memes.svg"
+      },
+      {
+        title: "chibirdart",
+        src: "assets/img/chibirdart.svg"
+      },
+      {
+        title: "razoesparaacreditar",
+        src: "assets/img/razoesparaacreditar.svg"
+      },
+      {
+        title: "adorable_animals",
+        src: "assets/img/adorable_animals.svg"
+      },
+      {
+        title: "smallcutecats",
+        src: "assets/img/smallcutecats.svg"
+      },
+    ]
+
+
     return(
         <div class="sidebar">
-          <Usuario name="Catana" boldname="catanacomics" image="assets/img/catanacomics.svg"/>
+          {itemsUsuario.map(item => <Usuario name={item.name} boldname={item.boldname} image={item.image} />)}
 
           <div class="sugestoes">
             <div class="titulo">
               Sugestões para você
               <div>Ver tudo</div>
             </div>
-                <Sugestao title="bad.vibes.memes" src="assets/img/bad.vibes.memes.svg" />
-                <Sugestao title="chibirdart" src="assets/img/chibirdart.svg" />
-                <Sugestao title="razoesparaacreditar" src="assets/img/razoesparaacreditar.svg" />
-                <Sugestao title="adorable_animals" src="assets/img/adorable_animals.svg" />
-                <Sugestao title="smallcutecats" src="assets/img/smallcutecats.svg" />
+                {itemsSugestao.map(itemsug => <Sugestao title={itemsug.title} src={itemsug.src} />)}
           </div>
 
           <div class="links">
