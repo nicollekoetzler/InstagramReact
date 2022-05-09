95ed7af7918cb68e506b791328de057d84a30cd0
Nicolle <nickkoetzler@email.com>
2022-05-09 16:11:39 -0300

    ermina parte dinâmica posts

--- src/Stories.js
+++ src/Stories.js
@@ -1,23 +1,47 @@
 import Story from "./Story"
 
 export default function Stories(){
+
+
+    const itemsStories = [
+        {
+          name: "9gag",
+          image: "assets/img/9gag.svg",
+        },
+        {
+          name: "meowed",
+          image: "assets/img/meowed.svg",
+        },
+        {
+          name: "barked",
+          image: "assets/img/barked.svg",
+        },
+        {
+          name: "nathanwpylestrangeplanet",
+          image: "assets/img/nathanwpylestrangeplanet.svg",
+        },
+        {
+          name: "wawawicomics",
+          image: "assets/img/wawawicomics.svg",
+        },
+        {
+          name: "respondeai",
+          image: "assets/img/respondeai.svg",
+        },
+        {
+          name: "filomoderna",
+          image: "assets/img/filomoderna.svg",
+        },
+        {
+          name: "memeriagourmet",
+          image: "assets/img/memeriagourmet.svg",
+        },
+    ]
+
+
     return(
         <div class="stories">
-            <Story name="9gag" image="assets/img/9gag.svg" />
-
-            <Story name="meowed" image="assets/img/meowed.svg" />
-
-            <Story name="barked" image="assets/img/barked.svg" />
-
-            <Story name="nathanwpylestrangeplanet" image="assets/img/nathanwpylestrangeplanet.svg" />
-
-            <Story name="wawawicomics" image="assets/img/wawawicomics.svg" />
-            
-            <Story name="respondeai" image="assets/img/respondeai.svg" />
-
-            <Story name="filomoderna" image="assets/img/filomoderna.svg" />
-
-            <Story name="memeriagourmet" image="assets/img/memeriagourmet.svg" />
+            {itemsStories.map(item => <Story name={item.name} image={item.image} />)}
 
             <div class="setinha">
               <ion-icon name="chevron-forward-circle"></ion-icon>
